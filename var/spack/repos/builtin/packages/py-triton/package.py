d07f3fb9883b36410a0e81a2f6534e6beb55ef05
aurianer <aurianer@cscs.ch>
2024-06-18 15:14:35 +0200

    Update py-triton

--- var/spack/repos/builtin/packages/py-triton/package.py
+++ var/spack/repos/builtin/packages/py-triton/package.py
@@ -18,8 +18,20 @@ class PyTriton(PythonPackage):
     version("main", branch="main")
     version("2.1.0", sha256="4338ca0e80a059aec2671f02bfc9320119b051f378449cf5f56a1273597a3d99")
 
-    depends_on("py-setuptools@40.8:", type="build")
+    depends_on("py-setuptools@66.8:", type="build")
     depends_on("cmake@3.18:", type="build")
     depends_on("py-filelock", type=("build", "run"))
+
+    conflicts("^llvm~lld", when="%clang")
+
+# TODO: potentially require clang if gcc not working
+
+#    with default_args(type=("build", "run")):
+#        depends_on("py-torch")
+
+    def setup_build_environment(self, env):
+        """Set environment variables used to control the build"""
+        if "%clang" in self.spec:
+            env.set("TRITON_BUILD_WITH_CLANG_LLD", "True")
 
     build_directory = "python"
