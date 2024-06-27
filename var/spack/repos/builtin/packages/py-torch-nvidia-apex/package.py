74e0aa605e8c3ff6c33ad63db776cf11b6e9b6a3
aurianer <aurianer@cscs.ch>
2024-06-27 16:21:32 +0200

    Add fine grained options for nvidia apex

--- var/spack/repos/builtin/packages/py-torch-nvidia-apex/package.py
+++ var/spack/repos/builtin/packages/py-torch-nvidia-apex/package.py
@@ -28,6 +28,27 @@ class PyTorchNvidiaApex(PythonPackage, CudaPackage):
 
     variant("cuda", default=True, description="Build with CUDA")
 
+    variant("bnp", default=True)
+    variant("cudnn_gbn_lib", default=True)
+    variant("fast_layer_norm", default=True)
+    variant("fast_multihead_attn", default=True)
+    variant("fmhalib", default=True)
+    variant("focal_loss_cuda", default=True)
+    variant("fused_conv_bias_relu", default=True)
+    variant("fused_index_mul_2d", default=True)
+    variant("index_mul_2d", default=True)
+    variant("permutation_search_cuda", default=True)
+    variant("peer_memory", default=True)
+    variant("permutation_search_cuda", default=True)
+    variant("nccl_p2p", default=True)
+    variant("transducer", default=True)
+    variant("xentropy", default=True)
+
+    requires("nccl@2.10:", when="+nccl_p2p")
+    requires("cudnn@8.5:", when="+cudnn_gbn_lib")
+    requires("cudnn@8.4:", when="+fused_conv_bias_relu")
+    depends_on("cuda@11", when="+cuda")
+
     # https://github.com/NVIDIA/apex/issues/1498
     # https://github.com/NVIDIA/apex/pull/1499
     patch("1499.patch", when="@2020-10-19")
@@ -43,8 +64,33 @@ class PyTorchNvidiaApex(PythonPackage, CudaPackage):
         args = []
         if spec.satisfies("^py-torch@1.0:"):
             args.append("--cpp_ext")
-            if "+cuda" in spec:
+            if spec.satisfies("+cuda"):
                 args.append("--cuda_ext")
+
+            if spec.satisfies("+bnp"):
+                args.append("--bnp")
+            if spec.satisfies("+cudnn_gbn_lib"):
+                args.append("--cudnn_gbn")
+            if spec.satisfies("+fast_layer_norm"):
+                args.append("--fast_layer_norm")
+            if spec.satisfies("+fmhalib"):
+                args.append("--fmha")
+            if spec.satisfies("+fast_multihead_attn"):
+                args.append("--fast_multihead_attn")
+            if spec.satisfies("+permutation_search_cuda"):
+                args.append("--permutation_search")
+            if spec.satisfies("+focal_loss_cuda"):
+                args.append("--focal_loss")
+            if spec.satisfies("+xentropy"):
+                args.append("--xentropy")
+            if spec.satisfies("+fused_index_mul_2d"):
+                args.append("--index_mul_2d")
+            if spec.satisfies("+transducer"):
+                args.append("--transducer")
+            if spec.satisfies("+peer_memory"):
+                args.append("--peer_memory")
+            if spec.satisfies("+fused_conv_bias_relu"):
+                args.append("--fused_conv_bias_relu")
         return args
 
     @when("^python@3.11:")
